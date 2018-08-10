b8367067ab4ceb193ef154cf5419670de93fd259
Chakradhar Kasturi <chakradhar.kasturi@amadeus.com>
2018-08-10 18:34:19 +0530

    initial code push

---
+++ firebase-messaging-sw.js
@@ -0,0 +1,21 @@
+importScripts('https://www.gstatic.com/firebasejs/5.2.0/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/5.2.0/firebase-messaging.js');
+
+console.log("Registered SW")
+firebase.initializeApp({
+    'messagingSenderId': '990720914069'
+});
+
+const messaging = firebase.messaging();
+messaging.setBackgroundMessageHandler(function (payload) {
+    console.log('[firebase-messaging-sw.js] Received background message ', payload);
+    // Customize notification here
+    var notificationTitle = 'Background Message Title';
+    var notificationOptions = {
+        body: 'Background Message body.',
+        icon: '/firebase-logo.png'
+    };
+
+    return self.registration.showNotification(notificationTitle,
+        notificationOptions);
+});
---
+++ send-notification.js
@@ -0,0 +1,29 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyCGTT2wpVE44VO9Fm1nkOWXLrF4hfiFW3k",
+    authDomain: "digitalhify-65472.firebaseapp.com",
+    databaseURL: "https://digitalhify-65472.firebaseio.com",
+    projectId: "digitalhify-65472",
+    storageBucket: "digitalhify-65472.appspot.com",
+    messagingSenderId: "990720914069"
+};
+
+firebase.initializeApp(config);
+
+const messaging = firebase.messaging();
+messaging.usePublicVapidKey("BGkQP_MY_r0cMHbcVHlkdDI_VPhbNhWB4HPP_OXNQfVmce-8mDdXrq8DZ434t9G2PTwKb2WlcJQImVUblTKJ-1I");
+
+messaging
+    .requestPermission()
+    .then(function () {
+        console.log("Permission accepted");
+        return messaging.getToken();
+    })
+    .then(function (data) {
+        localStorage.setItem("dh_token", data);
+        console.log(data)
+});
+// messaging on receive for foreground
+messaging.onMessage(function(payload){
+    console.log(payload);
+});
