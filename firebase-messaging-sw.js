c08490d0f1a7a6a42891f8173c98139cd2df6d80
chakri janvhi <chakrijanvhi@chakradhars-MacBook-Air.local>
2018-08-26 16:52:51 +0530

    check

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,26 +1,9 @@
-importScripts('https://www.gstatic.com/firebasejs/4.9.1/firebase-app.js')
-importScripts('https://www.gstatic.com/firebasejs/4.9.1/firebase-messaging.js')
+importScripts('https://www.gstatic.com/firebasejs/5.3.1/firebase-app.js')
+importScripts('https://www.gstatic.com/firebasejs/5.3.1/firebase-messaging.js')
 
-console.log("Registered SW")
 firebase.initializeApp({
     'messagingSenderId': '990720914069'
 });
-
-const messaging = firebase.messaging();
-messaging.setBackgroundMessageHandler(function (payload) {
-        console.log("payload")
-        console.log(payload)
-});
-// messaging.setBackgroundMessageHandler(function (payload) {
-//     console.log('[firebase-messaging-sw.js] Received background message ', payload);
-//     // Customize notification here
-//     var notificationTitle = 'Background Message Title';
-//     var notificationOptions = {
-//         body: 'Background Message body.'
-//     };
-//     return self.registration.showNotification(notificationTitle,
-//         notificationOptions);
-// });
 
 self.addEventListener('push',function(e){
     const data = e.data.json()
